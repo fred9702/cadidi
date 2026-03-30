edc578630944321f5b33cd4073cbf6cbe0580b38
fred9702 <fred9702@gmail.com>
2026-03-30 12:40:58 +0100

    feat: add Supabase client initialisation

---
+++ app/supabase_client.py
@@ -0,0 +1,10 @@
+from supabase import create_client, Client
+from decouple import config
+
+
+def get_supabase() -> Client:
+    """Create and return a Supabase client."""
+    return create_client(
+        config("SUPABASE_URL"),
+        config("SUPABASE_KEY"),
+    )
